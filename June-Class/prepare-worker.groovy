9f98f77faac27d14121591bc6e772f5ca50a93c6
azizbekmol <azizmdov@gmail.com>
2021-02-04 18:29:14 +0000

    configured prepare-worker

--- June-Class/prepare-worker.groovy
+++ June-Class/prepare-worker.groovy
@@ -1,7 +1,12 @@
+properties([
+    parameters([
+        string(defaultValue: '', description: 'Input node IP', name: 'SSHNODE', trim: true)
+        ])
+    ])
 node {
     stage("Initialize") {
         withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@204.48.26.182 yum install epel-release -y"
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@$params.SSHNODE yum install epel-release -y"
         }
     }
 }
