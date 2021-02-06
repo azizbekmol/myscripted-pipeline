72598ee5055e178a291fefa8010b4581daf20e83
azizbekmol <azizmdov@gmail.com>
2021-02-06 17:33:23 +0000

    Updated

--- install-melodi.groovy
+++ install-melodi.groovy
@@ -5,15 +5,17 @@ properties([
     ])
 
 node {
-    stage("Pull Repo"){
+    stage("PullRepo"){
         sh "rm -rf ansible-melodi && git clone https://github.com/azizbekmol/ansible-melodi.git"
     }
+
+
     withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
         stage("Install Melodi"){
             sh """
                 export ANSIBLE_HOST_KEY_CHECKING=False
                 ansible-playbook -i "${params.node}," --private-key $SSHKEY ansible-melodi/main.yml -b -u $SSHUSERNAME
             """
-        }
+        }    
     }
 }
