7d73316a202c97b7edaca04b24bedbf049096441
azizbekmol <azizmdov@gmail.com>
2021-02-07 06:59:17 +0000

    using ansible plugins

--- June-Class/install-melodi.groovy
+++ June-Class/install-melodi.groovy
@@ -8,12 +8,8 @@ node {
     stage("Pull Repo"){
         sh "rm -rf ansible-melodi && git clone https://github.com/azizbekmol/ansible-melodi.git"
     }
-    withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
-        stage("Install Melodi"){
-            sh """
-                export ANSIBLE_HOST_KEY_CHECKING=False
-                ansible-playbook -i "${params.node}," --private-key $SSHKEY ansible-melodi/main.yml -b -u $SSHUSERNAME
-            """
-        }
+
+    stage("Install Melodi"){
+        ansiblePlaybook become: true, colorized: true, credentialsId: 'jenkins-master', disableHostKeyChecking: true, inventory: "${params.node},", playbook: 'ansible-melodi/main.yml'
     }
 }
